3ab02fd206194cee11ffc4d5628f3d8863b1c537
drnlerum-glitch <drnlerum@gmail.com>
2026-08-10 22:01:30 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,1 +1,39 @@
+// for(let i = 0; i <= 5; i++) {
+// console.log(i);
+//  }
+//Write a for loop to print numbers from 1 to 10.
+// for( let i=1; i<=10; i++){
+
+//     console.log("i=: ", i);
+// }
+// Write a for loop to print only the even numbers from 1 to 20
+// for( let i=1; i<=20; i++){
+//     if(i % 2 !== 0) continue 
+//     console.log("i=: ", i);
+// }
+
+// Write a for loop to print numbers in reverse from 10 to 1.
+
+// for(let i = 10; i>0; i--){
+// console.log("i:= ", i);
+
+// }
+
+// Write a while loop to print numbers from 1 to 5.
+// let k = 1;
+// while(k <= 5){
+//     console.log("k =: ", k );
+//     k++;
+// }
+// Write a while loop that stops when the counter reaches 3.
+for(let i = 1; i<=10; i++){
+    console.log("i:= ", i);
+    if(i>=3){
+        //console.log("i =: ", i);
+        console.log("Loop terminated at i:= ", i);
+         break;
+    }
+       
+    }
+
 
